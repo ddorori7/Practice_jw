ca206a9a00e5c56886c997dae68ec54a0793461f
82105 <82105@DESKTOP-HKT2CAB>
2021-07-24 20:51:19 +0900

    생성자 호출 오버로딩 연습

---
+++ src/song/Song1.java
@@ -0,0 +1,53 @@
+package song;
+
+public class Song1 {
+
+	private String title;
+	private String artist;
+	private String album;
+	private String composer;
+	private int year;
+	private String track;
+	
+	
+	public Song1() {
+	} // 기본 생성자
+	
+
+	public Song1(String title, String artist) { // 생성자 오버로딩
+		this(title, artist, null, 0, "3번 track", "이민수 작곡"); // 다른 생성자 호출
+//		this.title = "아이유";
+//		this.artist = "좋은날";
+	}
+	
+//	public Song1(String title, String artist) { // 생성자 오버로딩 똑같이 하면 에러생김
+//		this(title, artist, null, 0, null, null);
+//	}
+
+	
+	public Song1(String title, String artist,
+			String album, int year, String composer, String track) {
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+		this.composer = composer;
+		this.year = year;
+		this.track = track;
+	} // 모든 필드 초기화 생성자?
+	
+	
+	public void showInfo() {
+		System.out.printf("%s, %s ( %s, %d, %s, %s )%n", 
+				artist, 
+				title, 
+				album, 
+				year,
+				track,
+				composer);
+	}
+	
+	
+	
+	
+	
+}
--- src/song/SongAPP.java
+++ src/song/SongAPP.java
@@ -3,14 +3,26 @@ package song;
 public class SongAPP {
 
 	public static void main(String[] args) {
-		Song song1 = new Song("아이유", "좋은날", "Real", 2010, "3번 track", "이민수 작곡");
-		Song song2 = new Song("BIGBANG", "거짓말", "Always", 2007, "2번 track", "G-DRAGON 작곡");
-		Song song3 = new Song("버스커버스커", "벚꽃엔딩", "버스커버스커1집", 2012, "4번 track", "장범준 작곡");
+//		Song song1 = new Song("아이유", "좋은날", "Real", 2010, "3번 track", "이민수 작곡");
+//		Song song2 = new Song("BIGBANG", "거짓말", "Always", 2007, "2번 track", "G-DRAGON 작곡");
+//		Song song3 = new Song("버스커버스커", "벚꽃엔딩", "버스커버스커1집", 2012, "4번 track", "장범준 작곡");
+//		
+//		
+//		song1.showInfo();
+//		song2.showInfo();
+//		song3.showInfo();
+		
+		Song1 song1 = new Song1();
+		song1.showInfo(); // 기본 생성자 테스트
+		
+		Song1 song2 = new Song1("아이유", "좋은날", "Real", 
+				2010, "3번 track", "이민수 작곡");
+		song2.showInfo(); // 모든 필드 초기값 생성자 테스트
+		
+		Song1 song3 = new Song1("아이유", "좋은날");
+		song3.showInfo(); // 생성자 호출 테스트
 		
 		
-		song1.showInfo();
-		song2.showInfo();
-		song3.showInfo();
 	}
 
 }
