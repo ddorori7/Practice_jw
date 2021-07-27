441fe13d9d107cba9bb57196d260ec59b3914ff1
서지원 <qrd7079@gmail.com>
2021-07-27 21:51:28 +0900

    Person초기 세팅

--- src/oop_programming/person/Student.java
+++ src/oop_programming/person/Student.java
@@ -2,4 +2,37 @@ package oop_programming.person;
 
 public class Student extends Person {
 
+	String schoolName;
+	
+	Student(String schoolName){
+		super();
+		this.schoolName = schoolName;
+	}
+	
+	Student(String name, int age, String schoolName) {
+		super(name, age);
+		this.schoolName = schoolName;
+	}
+	
+	
+
+	public String getSchoolName() {
+		return schoolName;
+	}
+
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+
+
+	public void showInfo() {
+		Student s = null;
+		name = s.getName();
+		age = s.getAge();
+		schoolName = s.getSchoolName();
+		System.out.println(s.toString());
+		
+	}
+	
+	
 }
